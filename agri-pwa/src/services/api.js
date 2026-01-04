57f616efaae2aee2527b26104c051a8331562d6d
mike23700 <kengnem070@gmail.com>
2026-01-04 19:56:45 +0100

    Update API base URL to use environment variable

--- agri-pwa/src/services/api.js
+++ agri-pwa/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.136:8080",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 export default api;
